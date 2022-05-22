f8c4896363fcef3ad1f488d3352b6931dd9a9ec3
Muzammil803 <syedmuzammil691@gmail.com>
2022-05-23 02:31:48 +0500

    assignment 02 completed

---
+++ app.js
@@ -0,0 +1,204 @@
+
+
+// chap 03
+
+// var age = "i am 17 years old"
+// alert (age)
+// var visitedtime = "you have visited this web 1st time"
+// alert (visitedtime)
+// document.write("my birth year is 2004 ")
+
+//chap 04
+ // question no 1
+
+// var a = "1"
+// var b = "2"
+// var c = "3"
+// alert(a+b+c)
+
+// question 02
+
+// var topic = "5 legal variable name\na\nb\nc\nd\ne"
+
+// alert(topic)
+// var topic = "5 legal variable name\nalert\nvar\nif\nconsole\ndocument"
+
+// alert(topic)
+
+//question no 4
+// document.write("<h1>rules for naming js variable </h1> </br> <p> variable name can only contain, number, names  </p> </br> <p> variable must begin with a name , small letter  </p> </br> <p> variable name are case sensitive  </p> </br> <p> variable name should not be js keywords  </p>   " ) 
+
+// chap 05
+
+// question 01 and 02
+
+// var a = 4
+// var b = 8
+// document.write("sum of 4 and 8 is " )
+// document.write( a+b )
+// var a = 4
+// var b = 8
+// document.write("subtraction of 4 and 8 is " )
+// document.write( a-b )
+// var a = 4
+// var b = 8
+// document.write("product of 4 and 8 is " )
+// document.write( a*b )
+// var a = 4
+// var b = 8
+// document.write("division of 4 and 8 is " )
+// document.write( a/b )
+
+// question 03
+
+// var a = 2 
+// document.write(a )
+// document.write("</br>")
+// document.write("value after increament is ")
+
+// document.write( ++a)
+
+// document.write("</br> value after adding 6 is ")
+// document.write ( a+6)
+// document.write("</br> value after decrement is ")
+// document.write( --a )
+// document.write("</br> Reminder after dividing by 2 is   ")
+// document.write( "0")
+
+//question 04
+
+// var ticket = 600
+// document.write("total coast to buy 5 movie is ")
+// document.write( ticket*5)
+// document.write("PKR")
+
+//question 05
+// var a = 4
+// document.write("<h1> Table of 4</h1>")
+// document.write("</br><h4>4x1= </h4> ")
+// document.write(a*1)
+// document.write("</br><h4>4x2= </h4> ")
+// document.write(a*2)
+// document.write("</br><h4>4x3= </h4> ")
+// document.write(a*3)
+// document.write("</br><h4>4x4= </h4> ")
+// document.write(a*4)
+// document.write("</br><h4>4x5= </h4> ")
+// document.write(a*5)
+// document.write("</br><h4>4x6= </h4> ")
+// document.write(a*6)
+// document.write("</br><h4>4x7= </h4> ")
+// document.write(a*7)
+// document.write("</br><h4>4x8= </h4> ")
+// document.write(a*8)
+// document.write("</br><h4>4x9= </h4> ")
+// document.write(a*9)
+// document.write("</br><h4>4x10= </h4> ")
+// document.write(a*10)
+
+// question 06 
+
+// var C = 30
+// var cformula = (C*9/5)+32 
+// document.write("30C is ")
+// document.write(cformula)
+// document.write("F")
+
+// document.write("</br>")
+// var F = 98
+// var fformula = (F-32)*5/9 
+// document.write("98F is ")
+// document.write(fformula)
+// document.write("C")
+
+//question 07
+// var item1 = 200
+// var item2 = 100
+// var shipingfee = 50
+// document.write("price of juice is  ")
+// document.write(item1)
+
+// document.write("</br>")
+// document.write("quantity of juice is 4")
+// document.write("</br>")
+
+
+
+// document.write("price of biscuit is  ")
+// document.write(item2)
+
+// document.write("</br>")
+// document.write("quantity of biscuit is 6")
+// document.write("</br>")
+// document.write("</br>")
+
+// document.write("total cost of your order is")
+
+// document.write(item1*4 + item2*6 + shipingfee)
+
+
+//question 08
+
+
+// var obtain = 515
+// var total = 550
+
+// document.write("<h1>Marks sheet </h1>")
+// document.write("</br>")
+// document.write("</br>")
+// document.write("</br>")
+
+// document.write("Total Marks: ")
+// document.write(total)
+// document.write("</br>")
+// document.write("Mraks obtained: ")
+// document.write(obtain)
+// document.write("</br>")
+// document.write("Percentage: ")
+
+
+// document.write((obtain/total)*100 )
+
+//question 09
+
+
+// var riyal=53.44
+// var usd=200.45
+// document.write("Total currency in PKR= ")
+// document.write(10*usd + 25*riyal)
+
+//question 10
+
+// var a=6
+// alert(((a+5)*10)/2)
+
+//question 11
+
+// var currentyear=2022
+// var birthyear=2004
+
+// document.write("Current year: ")
+// document.write(currentyear)
+// document.write("</br> ")
+// document.write("birth year: ")
+// document.write(birthyear)
+// document.write("</br>")
+// document.write("your age: ")
+// document.write(currentyear-birthyear)
+
+
+// question 12
+
+// var r=25
+// document.write("<h1>THE GEOMETRIZER </h1>")
+// document.write("</br>")
+// document.write("radius of a circle is ")
+// document.write(25)
+// document.write("</br>")
+// document.write("The circumference is ")
+// document.write(2*3.142*r)
+// document.write("</br>")
+// document.write("The area is ")
+// document.write(3.142*r*r)
+
+
